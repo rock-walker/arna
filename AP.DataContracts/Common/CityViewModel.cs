eb8fbf7909f26cac621a4bfe4510687a6dfbd7ff
Andrei Melnikau <melnikov.work@gmail.com>
2017-07-24 22:44:01 +0300

    deleted DataContracts folder

--- AP.DataContracts/Common/CityViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AP.ViewModel.Common
-{
-    public class CityViewModel
-    {
-        public string Name { get; set; }
-        public string GoogleCode { get; set; }
-        public string Ru { get; set; }
-    }
-}
--- AP.DataContracts/Common/LocationViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AP.ViewModel.Common
-{
-    public class LocationViewModel
-    {
-        public double Lng { get; set; }
-        public double Lat { get; set; }
-    }
-}
